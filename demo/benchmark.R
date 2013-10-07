ef9136849cbc1ab26fa97c4fa12ab7591faa45c8
Dirk Eddelbuettel <edd@debian.org>
2013-10-06 20:26:19 -0500

    more comments

--- demo/benchmark.R
+++ demo/benchmark.R
@@ -6,13 +6,13 @@ v <- vector(mode="numeric", length=N)
 
 res <- benchmark(zrnormMT(N),       # Marsgalia and Tsang, JSS, 2000
                  zrnormLZLLV(N),    # Leong, Zhang et al, JSS, 2005
-                 zrnormV1(N),
+                 zrnormV1(N),       # based on initial Burkardt implementation
                  zrnormVecV1(v),    # fill a pre-supplied vector
                  #zrnormStlV1(N),   # fill STL vector
-                 rnorm(N),
-                 zrnorm(N),
+                 rnorm(N),          # R as a baseline
+                 zrnorm(N),         # based on updated Burkardt implementation
                  zrnormVec(v),      # fill a pre-supplied vector
                  #zrnormStl(N),     # fill STL vector
-                 zrnormgsl(N),
+                 zrnormgsl(N),      # GSL's ziggurat by Voss
                  replications=1000, order="relative")
 print(res[,1:4])
