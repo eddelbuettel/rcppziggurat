4973fb364efce060977353c510f6a3afc0cd94fc
Dirk Eddelbuettel <edd@debian.org>
2013-10-19 17:42:52 -0500

    minor edits

--- demo/normalTest.R
+++ demo/normalTest.R
@@ -1,16 +1,16 @@
 
 ## This modifies the approach in (R)DieHarder which does
 ##   take N draws from a U(0,1)
-##   repeat M times a
+##   repeat M times
 ##   and for large enough N, then the sum of all N draws goes to
 ##       mean   --> N/2
 ##       stddev --> sqrt(N/12)
 ##   which is known as the Irwin-Hall distribution
-##   the for each of the M values we can use the inverse of the normal to a p-value
+##   then for each of these M values use the inverse of normal to obtain a p-value
 ##   that p value should be uniformly distributed across these M draws
-##   so use Kuiper's K/S test variant
+##   so use Kuiper's K/S test variant to test for uniform U(0,1)
 ##
-## Here we don;t need Irwin-Hall: the sum of N vars drawn as N(0,1) will be N(0,sqrt(N))
+## Here we don't need Irwin-Hall: the sum of N vars drawn as N(0,1) will be N(0,sqrt(N))
 ## So we compute a p value from that and assemple M such p values
 
 library(RcppZiggurat)
