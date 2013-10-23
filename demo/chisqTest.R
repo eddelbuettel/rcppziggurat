0dce52bbf19e47864c8a4b1ce497761936509980
Dirk Eddelbuettel <edd@debian.org>
2013-10-22 20:50:18 -0500

    per generator plots

--- demo/chisqTest.R
+++ demo/chisqTest.R
@@ -8,7 +8,7 @@ res <- chisqTest(draws=1e8,    		# individual draws
                  bins=bins,  		# repeats pre draw
                  edge=7, 		# cutoff for binning at +/- edge
                  seed=123456789,
-                 res=50,       	# resolution (number of rows until draws)
+                 res=50,  	     	# resolution (number of rows until draws)
                  generators=c("Ziggurat", "MT", "LZLLV", "GSL", "V1", "V1b"))
 
 cval <- qchisq(0.95, bins-1)
@@ -16,5 +16,9 @@ cat("Actual chisq(", bins, ") values\n")
 cat("Critical one-sided 95% value is ", cval, "\n")
 print(tail(res,1))
 
-matplot(res, type='l')
-abline(h=cval, col="darkgrey", lty="dotted")
+op <- par(mfrow=c(2,3), mar=c(3,3,3,1))
+for (i in seq_len(ncol(res))) {
+    plot(res[,i], type='l', ylim=c(0,max(res)), main=colnames(res)[i])
+    abline(h=cval, col="darkgrey", lty="dotted")
+}
+par(op)
