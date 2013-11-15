45d9e4c16b3cff3d6a9118b64f633d723c8bbb14
Dirk Eddelbuettel <edd@debian.org>
2013-11-14 19:39:37 -0600

    set xlim and ylim

--- R/plotTest.R
+++ R/plotTest.R
@@ -22,7 +22,7 @@ plotTest <- function(v, g) {
     pks <- ks.test(v, "punif", 0, 1, exact=TRUE)$p.value
     pw <- wilcox.test(v, mu=0.5)$p.value
 
-    plot(ecdf(v), verticals=TRUE, do.p=FALSE,
+    plot(ecdf(v), verticals=TRUE, do.p=FALSE, xlim=c(0,1), ylim=c(0,1),
          #main=paste0(g, " pKS: ", round(pks, digits=4), " pWil.: ", round(pw, digits=4)))
          main=g)
     segments(0,0,1,1, col='darkgray', lty="dotted")
